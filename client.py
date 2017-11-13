ae7e0ba61e2846711a46a3fde15ece4b2932bcbb
Benjamin Collet <ben@clt.tf>
2017-11-13 21:17:35 +0100

    Adding support for multiple instances

--- client.py
+++ client.py
@@ -7,9 +7,13 @@ import argparse
 with open("config.yml", "r") as ymlfile:
     cfg = yaml.load(ymlfile)
 
-def call_api(path):
-    url = '/'.join((cfg['base_url'],cfg['api_path'],path))
-    response = requests.get(url, auth=(cfg['username'], cfg['password']))
+def call_api(params, path):
+    url = '/'.join((params['base_url'],params['api_path'],path))
+    try:
+        response = requests.get(url, auth=(params['username'], params['password']))
+    except:
+        return
+
     return response.json()
 
 def print_data(key, value, suffix = None):
@@ -18,30 +22,41 @@ def print_data(key, value, suffix = None):
     else:
         print "%-20s %s" % (key, value)
 
+# Test instances
+for instance in cfg['instances'].keys():
+    test = call_api(cfg['instances'][instance], 'devices')
+    if test is None:
+        print "Instance %s is not working, disabling" % instance
+        cfg['instances'].pop(instance)
+    else:
+        print "Instance %s is working" % instance
+
 def search_ports(args):
-    data = call_api('ports/?ifAlias=%s' % args.string)
+    for instance, params in cfg['instances'].iteritems():
+        data = call_api(params, 'ports/?ifAlias=%s' % args.string)
 
-    if data['count'] < 1:
-        print "No port found matching description %s" % args.string
-        return
+        if data['count'] < 1:
+            print "No port found for description %s on instance %s" \
+                % (args.string, instance)
+            return
 
-    for port in data['ports'].itervalues():
-        print "=" * 80
-        device = call_api('devices/%s' % port['device_id'])
-        print_data("Device", device['device']['hostname'])
-        print_data("", "%s/device/device=%s" % (cfg['base_url'], port['device_id']))
-        print_data("Port", port['port_label_short'])
-        print_data("", "%s/device/device=%s/tab=port/port=%s/" %
-            (cfg['base_url'], port['device_id'], port['port_id']))
-        print_data("Description", port['ifAlias'])
-        print_data("Port status", "%s (Admin) / %s (Oper)" % (port['ifAdminStatus'], port['ifOperStatus']))
-        print_data("Last flap", port['ifLastChange'])
-        print_data("Speed", port['ifHighSpeed'], "Mbps")
-        print_data("Duplex", port['ifDuplex'])
-        print_data("Input rate", port['ifInOctets_rate'], "octets")
-        print_data("Output rate", port['ifOutOctets_rate'], "octets")
-        print_data("Input errors rate", port['ifInErrors_rate'])
-        print_data("Output errors rate", port['ifOutErrors_rate'])
+        for port in data['ports'].itervalues():
+            print "=" * 80
+            device = call_api(params, 'devices/%s' % port['device_id'])
+            print_data("Device", device['device']['hostname'])
+            print_data("", "%s/device/device=%s" % (params['base_url'], port['device_id']))
+            print_data("Port", port['port_label_short'])
+            print_data("", "%s/device/device=%s/tab=port/port=%s/" %
+                (params['base_url'], port['device_id'], port['port_id']))
+            print_data("Description", port['ifAlias'])
+            print_data("Port status", "%s (Admin) / %s (Oper)" % (port['ifAdminStatus'], port['ifOperStatus']))
+            print_data("Last flap", port['ifLastChange'])
+            print_data("Speed", port['ifHighSpeed'], "Mbps")
+            print_data("Duplex", port['ifDuplex'])
+            print_data("Input rate", port['ifInOctets_rate'], "octets")
+            print_data("Output rate", port['ifOutOctets_rate'], "octets")
+            print_data("Input errors rate", port['ifInErrors_rate'])
+            print_data("Output errors rate", port['ifOutErrors_rate'])
 
 # Argument parsing
 parser = argparse.ArgumentParser()
